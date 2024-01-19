a9730e193b51720075a0f79ec1f7aa642e71b7aa
kimcurly <sanbonai0728@gmail.com>
2024-01-19 12:16:01 +0900

    feat: 컨트롤러 매핑

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
-const helloRouter = require("./routes/hello");
+const signInRouter = require("./routes/signIn");
+const getUserRouter = require("./routes/getUser");
+const memberVerificationRouter = require("./routes/memberVerification");
+const registerBlackboxRouter = require("./routes/registerBlackbox");
+const userLoginRouter = require("./routes/userLogin");
 // const cors = require('cors')
 // const morgan = require('morgan')
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use("/hello", helloRouter);
+app.use("/sign-in", signInRouter);
+app.use("/get-user", getUserRouter);
+app.use("/member-verification", memberVerificationRouter);
+app.use("/register-blackbox", registerBlackboxRouter);
+app.use("/user-login", userLoginRouter);
 // app.use(cors())
 // app.use(morgan('combined'))
-
-app.get("/ping", function (req, res, next) {
-  res.json({ message: "pong" });
-});
 
 app.listen(3000, function () {
   console.log("server listening on port 3000");
