980c0e7dc2cc79dcfe41ca394a693e511e65395f
kimcurly <sanbonai0728@gmail.com>
2024-01-19 12:23:22 +0900

    라이브러리 추가

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const getUserRouter = require("./routes/getUser");
 const memberVerificationRouter = require("./routes/memberVerification");
 const registerBlackboxRouter = require("./routes/registerBlackbox");
 const userLoginRouter = require("./routes/userLogin");
-// const cors = require('cors')
-// const morgan = require('morgan')
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
@@ -16,8 +16,8 @@ app.use("/get-user", getUserRouter);
 app.use("/member-verification", memberVerificationRouter);
 app.use("/register-blackbox", registerBlackboxRouter);
 app.use("/user-login", userLoginRouter);
-// app.use(cors())
-// app.use(morgan('combined'))
+app.use(cors());
+app.use(morgan("combined"));
 
 app.listen(3000, function () {
   console.log("server listening on port 3000");
